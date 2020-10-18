a2d72bc4372a2ff0022f36360b35d44d26b4077e
karolina belzowska <belzowskakarolina@gmail.com>
2020-10-18 15:55:10 +0100

    exercises.python

---
+++ 2.py
@@ -0,0 +1,8 @@
+name = input("What is your name?")
+
+print("Hello",name,"," "how are you today?")
+
+
+name = raw_input("What is your name?")
+
+print("Hello %s, how are you?" %(name))
---
+++ 3.py
@@ -0,0 +1,17 @@
+name = input("What is your name?")
+
+if (name == "ALice") or (name == "Bob"):
+    print("Hello %s, How are you?" %(name))
+
+else:
+    print("Hello stranger!")
+
+
+
+
+name = input("What is your name?")
+
+if (name=="Bob") or (name=="Alice"):
+    print("Hello %s, How are you?" %(name))
+else:
+    print("Hello lowly peasant!")
